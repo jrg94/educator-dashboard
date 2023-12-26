ef988a3edb327f001648775408f6e18abab3f5ff
Jeremy Grifski <jeremy.grifski@gmail.com>
2023-12-26 02:19:21 -0500

    Created a generic homepage

--- pages/home.py
+++ pages/home.py
@@ -8,6 +8,15 @@ dash.register_page(
 )
 
 layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
+    html.H1('Home'),
+    html.P(
+        '''
+        Welcome to the new and improved educator dashboard! This website was previously a single
+        page website with tabs. As it grew, it felt necessary to expand the website into
+        logical pages. For instance, if you want to learn about how I have been evaluated as an
+        instructor, check out my evaluations page using the "Evaluations" link in the navigation bar.
+        Alternatively, if you're interested in any of the classes I teach, click their associated
+        links instead. At this time, the homepage is a work-in-progress. Happy browsing!
+        '''
+    ),
 ])
