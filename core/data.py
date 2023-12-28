7923fd2a908a22c5279a600cc79fb2d5a8d4597e
Jeremy Grifski <jeremy.grifski@gmail.com>
2023-12-27 21:37:40 -0500

    Cleaned up data formatting

--- core/data.py
+++ core/data.py
@@ -8,7 +8,7 @@ def load_assignment_survey_data() -> dcc.Store:
     """
     Loads the assignment survey data from the remote CSV, cleans it, and computes
     some important metrics. The result is returned as a store object.
-    
+
     :return: the assignment survey data as a store
     """
     assignment_survey_data = pd.read_csv(URL_ASSIGNMENT_SURVEY)
@@ -40,7 +40,7 @@ def load_assignment_survey_data() -> dcc.Store:
 def load_sei_data() -> dcc.Store:
     """
     Loads the SEI data from the remote CSV. The result is returned as a store object.
-    
+
     :return: the SEI data as a store
     """
     sei_data = pd.read_csv(URL_SEI_DATA)
@@ -50,7 +50,7 @@ def load_sei_data() -> dcc.Store:
 def load_sei_comments_data() -> dcc.Store:
     """
     Loads the SEI comment data from the remote CSV. The result is returned as a store object.
-    
+
     :return: the SEI comment data as a store 
     """
     sei_comment_data = pd.read_csv(URL_SEI_COMMENTS_DATA)
@@ -60,24 +60,24 @@ def load_sei_comments_data() -> dcc.Store:
 def load_course_eval_data() -> dcc.Store:
     """
     Loads the course evaluation data from the remote CSV. The result is returned as a store object.
-    
+
     :return: the SEI course evaluation data as a store
     """
     course_eval_data = pd.read_csv(URL_COURSE_EVAL_DATA)
 
     # Sets types of columns
     course_eval_data["Timestamp"] = pd.to_datetime(
-        course_eval_data["Timestamp"], 
+        course_eval_data["Timestamp"],
         format="%Y/%m/%d %I:%M:%S %p %Z"
     )
 
-    return dcc.Store(id=ID_COURSE_EVAL_DATA, data=course_eval_data.to_json())    
+    return dcc.Store(id=ID_COURSE_EVAL_DATA, data=course_eval_data.to_json())
 
 
 def load_cse2221_grade_data() -> dcc.Store:
     """
     Loads the grade data from the remote CSV. The result is returned as a store object. 
-    
+
     :return: the grade data as a store
     """
     grade_data = pd.read_csv(URL_CSE_2221_GRADE_DATA)
@@ -87,12 +87,15 @@ def load_cse2221_grade_data() -> dcc.Store:
 def load_cse2231_grade_data() -> dcc.Store:
     """
     Loads the grade data from the remote CSV. The result is returned as a store object. 
-    
+
     :return: the grade data as a store
     """
     grade_data = pd.read_csv(URL_CSE_2231_GRADE_DATA)
 
     # Sets types of columns
-    grade_data["Midterm Exam #1"] = pd.to_numeric(grade_data["Midterm Exam #1"], errors="coerce")
+    grade_data["Midterm Exam #1"] = pd.to_numeric(
+        grade_data["Midterm Exam #1"],
+        errors="coerce"
+    )
 
     return dcc.Store(id=ID_CSE_2231_GRADE_DATA, data=grade_data.to_json())
