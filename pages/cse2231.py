1ed7334db2454a916a2b6a98f42bff396a0c3d7e
Jeremy Grifski <jeremy.grifski@gmail.com>
2023-12-29 16:40:49 -0500

    Converting to constants

--- pages/cse2231.py
+++ pages/cse2231.py
@@ -18,7 +18,7 @@ dash.register_page(
 
 @callback(
     Output("cse2231-grade-overview", "figure"),
-    Input("cse2231-grade-data", "data")
+    Input(ID_CSE_2231_GRADE_DATA, "data")
 )
 def render_grade_overview_figure(jsonified_data):
     df = pd.read_json(StringIO(jsonified_data))
@@ -27,7 +27,7 @@ def render_grade_overview_figure(jsonified_data):
 
 @callback(
     Output("cse2231-homework-calculations", "figure"),
-    Input("cse2231-grade-data", "data")
+    Input(ID_CSE_2231_GRADE_DATA, "data")
 )
 def render_homework_calculations_figure(jsonified_data):
     df = pd.read_json(StringIO(jsonified_data))
@@ -36,7 +36,7 @@ def render_homework_calculations_figure(jsonified_data):
 
 @callback(
     Output("cse2231-project-calculations", "figure"),
-    Input("cse2231-grade-data", "data")
+    Input(ID_CSE_2231_GRADE_DATA, "data")
 )
 def render_project_calculations_figure(jsonified_data):
     df = pd.read_json(StringIO(jsonified_data))
@@ -45,7 +45,7 @@ def render_project_calculations_figure(jsonified_data):
 
 @callback(
     Output("cse2231-exams-calculations", "figure"),
-    Input("cse2231-grade-data", "data")
+    Input(ID_CSE_2231_GRADE_DATA, "data")
 )
 def render_exam_calculations_figure(jsonified_data):
     df = pd.read_json(StringIO(jsonified_data))
@@ -54,7 +54,7 @@ def render_exam_calculations_figure(jsonified_data):
 
 @callback(
     Output("cse2231-homework-time", "figure"),
-    Input("assignment-survey-data", "data")
+    Input(ID_ASSIGNMENT_SURVEY_DATA, "data")
 )
 def render_homework_time_figure(jsonified_data):
     df = pd.read_json(StringIO(jsonified_data))
@@ -62,21 +62,21 @@ def render_homework_time_figure(jsonified_data):
 
 
 @callback(
-    Output("cse2231-project-time", "figure"),
-    Input("assignment-survey-data", "data")
-)
-def render_project_time_figure(jsonified_data):
-    df = pd.read_json(StringIO(jsonified_data))
-    return create_time_fig(df, assignment="Project", course=FILTER_SOFTWARE_2)
-
-
-@callback(
     Output("cse2231-missing-homeworks", "figure"),
-    Input("cse2231-grade-data", "data")
+    Input(ID_CSE_2231_GRADE_DATA, "data")
 )
 def render_missing_homeworks_figure(jsonified_data):
     df = pd.read_json(StringIO(jsonified_data))
     return create_missing_assignment_fig(df, "Homework")
+
+
+@callback(
+    Output("cse2231-project-time", "figure"),
+    Input(ID_ASSIGNMENT_SURVEY_DATA, "data")
+)
+def render_project_time_figure(jsonified_data):
+    df = pd.read_json(StringIO(jsonified_data))
+    return create_time_fig(df, assignment="Project", course=FILTER_SOFTWARE_2)
 
 
 layout = html.Div([
